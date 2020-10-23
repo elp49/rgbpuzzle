19fa4d49a7c022a5029d1d1883943ed4ee632a6f
elp49 <elp49@drexel.edu>
2020-10-22 20:16:51 -0400

    adding breadth first search outline

--- agent.py
+++ agent.py
@@ -38,3 +38,54 @@ class Agent:
 
         # Return list of N states visited.
         return visited
+
+    @staticmethod
+    def bfs(state):
+        closed_nodes = []
+
+        # Store initial node in open node list.
+        open_nodes = [Node(state, None)]
+
+        # For each open node.
+        while len(open_nodes) > 0:
+            current_node = open_nodes[0]
+
+            # Test if current node is solution state.
+            if current_node.state.is_goal():
+                break
+
+            # Generate all possible next states from this node.
+            actions = current_node.state.actions()
+
+            # For each possible action on this state.
+            for a in actions:
+                # Clone the current state.
+                clone_state = current_node.state.clone()
+
+                # Execute this action on clone.
+                clone_state.execute(a)
+
+                #TODO: should it check here if this is also goal before pushing to back of open?
+
+                # Create child node and append to open node list.
+                child_node = Node(clone_state, current_node)
+                open_nodes.append(child_node)
+
+            # Remove closed node from open node list and append to closed list.
+            closed = open_nodes.pop(0)
+            closed_nodes.append(closed)
+
+        return current_node
+    
+
+    @staticmethod
+    def dfs(state):
+        return ''
+
+    
+
+    @staticmethod
+    def a_star(state, heuristic):
+        return ''
+
+    
